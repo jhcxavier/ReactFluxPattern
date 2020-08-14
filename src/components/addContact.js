3a227c0f487d3693c0533a5468eb2b04aa560c42
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-08-14 11:46:26 -0400

    features in addContact

--- src/components/addContact.js
+++ src/components/addContact.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 
 const AddContact = () => {
-    const [value, setValue] = useState()
+    const [value, setValue] = useState({
+        firstName: "",
+        lastName: "",
+        company: "",
+        email: "",
+        phone: ""
+    })
+
     return (
         <div>
             <div>
@@ -10,25 +17,28 @@ const AddContact = () => {
             <form className="container border mt-2">
                 <div className="row">
                     <div className="col m-2">
-                        <input type="text" className="form-control" placeholder="First name" />
+                        <input type="text" className="form-control" placeholder="First name" onChange={(e) => setValue({ ...value, firstName: e.target.value })} />
                     </div>
                     <div className="col m-2">
-                        <input type="text" className="form-control" placeholder="Last name" />
+                        <input type="text" className="form-control" placeholder="Last name" onChange={(e) => setValue({ ...value, lastName: e.target.value })} />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-5 m-2">
-                        <input type="text" className="form-control" placeholder="Company" />
+                        <input type="text" className="form-control" placeholder="Company" onChange={(e) => setValue({ ...value, company: e.target.value })} />
                     </div>
                     <div className="col m-2">
-                        <input type="text" className="form-control" placeholder="Email" />
+                        <input type="text" className="form-control" placeholder="Email" onChange={(e) => setValue({ ...value, email: e.target.value })} />
                     </div>
                     <div className="col m-2">
-                        <input type="text" className="form-control" placeholder="Phone" />
+                        <input type="text" className="form-control" placeholder="Phone" onChange={(e) => setValue({ ...value, phone: e.target.value })} />
                     </div>
                 </div>
                 <div className="row m-1 d-flex justify-content-end">
-                    <button type="submit" className="btn btn-primary m-1">Submit</button>
+                    <button type="submit" className="btn btn-primary m-1" onClick={(e) => {
+                        setValue("")
+                        // e.preventDefault()
+                    }}>Cancel</button>
                     <button type="submit" className="btn btn-primary m-1">Submit</button>
                 </div>
             </form>
