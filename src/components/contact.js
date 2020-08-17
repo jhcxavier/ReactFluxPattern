1bd10e862566c2efc1eeb5702c5bcc57123b6faa
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-08-17 15:25:05 -0400

    deactivating unused icons

--- src/components/contact.js
+++ src/components/contact.js
@@ -51,8 +51,8 @@ const Contact = ({ data }) => {
                                 <i id='tooltip' className="fas fa-edit p-2" samesite={"None"} type="button" onClick={() => {
                                     setEditContact(!editContact)
                                 }}></i>
-                                <i className="fas fa-users-cog p-2" samesite={"None"}></i>
-                                <i className="fas fa-file-download p-2" samesite={"None"}></i>
+                                <i className="fas fa-users-cog p-2 text-secondary" samesite={"None"}></i>
+                                <i className="fas fa-file-download p-2 text-secondary" samesite={"None"}></i>
                                 {/* Delete a contact */}
                                 <i className="far fa-trash-alt p-2" samesite={"None"} type="button" onClick={() => {
                                     actions.deleteContact(data._id)
